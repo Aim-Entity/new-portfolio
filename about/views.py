9063578a1480d42ef2b8ec6806e60d1a7fd7c407
Aim-Entity <59455267+Aim-Entity@users.noreply.github.com>
2020-10-23 15:12:30 +0100

    Added resume automate

--- about/views.py
+++ about/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Cv
 
 
 def about(request):
+    resume = Cv.objects.first()
     context = {
-
+        "resume": resume
     }
     return render(request, "about/about.htm", context)
